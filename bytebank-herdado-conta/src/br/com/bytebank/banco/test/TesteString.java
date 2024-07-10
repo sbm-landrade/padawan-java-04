23dda777ab45b0b861ba3c08b1b74ccd22723615
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-07-10 12:07:33 -0300

    Metodos da classe String

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
@@ -1,18 +1,43 @@
 package br.com.bytebank.banco.test;
 
+import java.util.Iterator;
+
 public class TesteString {
 
 	public static void main(String[] args) {
 
+		String vazio = "    Alura   ";
+		String outroVazio = vazio.trim();
+		
+		System.out.println(vazio.contains("Alu"));
+		System.out.println(outroVazio);
+		
 		String nome = "Alura";//object literal
 		//String outro = new String("Alura");
 		
-		//String outra = nome.replace("A", "a");
+		System.out.println(nome.length());
 		
-		String outra = nome.toLowerCase();
+		for (int i = 0; i < nome.length(); i++) {
+			System.out.println(nome.charAt(i));
+		}
 		
-		System.out.println(nome);
-		System.out.println(outra);
+//		String sub = nome.substring(1);
+//		System.out.println(sub);
+		
+//		int pos = nome.indexOf("ur");
+//		System.out.println(pos);
+		
+//		char c = nome.charAt(0);
+//		System.out.println(c);
+//		char c = 'A';
+//		char d = 'a';
+//		
+//		String outra = nome.replace(c, d);
+		
+		//String outra = nome.toUpperCase();
+		
+//		System.out.println(nome);
+//		System.out.println(outra);
 		
 	}
 
